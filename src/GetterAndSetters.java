1bc3a6a799d0f7b4f156e5b5d416125da1975123
DanielJ18 <atochala20@gmail.com>
2016-07-25 21:24:44 -0400

    Getters and Setters

---
+++ src/GetterAndSetters.java
@@ -0,0 +1,166 @@
+
+public class GetterAndSetters 
+
+{
+	private String[] productNames;
+
+	public void setProductNames(String[] productNames)
+	{
+	    this.productNames = productNames;
+	}
+
+	public String[] getProductNames()
+	{
+	    return productNames;
+	}
+	
+
+	
+	private double diskFileSize;
+
+	public double getDiskFileSize() {
+		return diskFileSize;
+	}
+
+	public void setDiskFileSize(double diskFileSize) {
+		this.diskFileSize = diskFileSize;
+	}
+	
+	
+	
+	private double landInAcres;
+
+	public double getLandInAcres() {
+		return landInAcres;
+	}
+
+	public void setLandInAcres(double landInAcres) {
+		this.landInAcres = landInAcres;
+	}
+	
+
+	
+	
+	private String nameOfFile;
+
+	public String getNameOfFile() {
+		return nameOfFile;
+	}
+
+	public void setNameOfFile(String nameOfFile) {
+		this.nameOfFile = nameOfFile;
+	}
+	
+	private String phoneNumber;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+
+
+	
+	private String companyName;
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	
+	
+	private String todaysDate;
+
+	public String getTodaysDate() {
+		return todaysDate;
+	}
+
+	public void setTodaysDate(String todaysDate) {
+		this.todaysDate = todaysDate;
+	}
+	
+	
+	
+	private boolean stateOfLightSwitch;
+
+	public boolean isStateOfLightSwitch() {
+		return stateOfLightSwitch;
+	}
+
+	public void setStateOfLightSwitch(boolean stateOfLightSwitch) {
+		this.stateOfLightSwitch = stateOfLightSwitch;
+	}
+	
+	
+	
+	private double pi;
+
+	public double getPi() {
+		return pi;
+	}
+
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+	
+	
+	
+	private double speedOfLightInVacuum;
+
+	public double getSpeedOfLightInVacuum() {
+		return speedOfLightInVacuum;
+	}
+
+	public void setSpeedOfLightInVacuum(double speedOfLightInVacuum) {
+		this.speedOfLightInVacuum = speedOfLightInVacuum;
+	}
+	
+
+	
+	
+	private Vehicle vehicle1;
+	
+	public Vehicle getVehicle1() {
+		return vehicle1;
+	}
+
+	public void setVehicle1(Vehicle vehicle1) {
+		this.vehicle1 = vehicle1;
+	}
+
+
+
+	
+	private Address address1;
+	
+	public Address getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(Address address1) {
+		this.address1 = address1;
+	}
+
+
+
+	private Product[] prodcut1;
+
+	public Product[] getProdcut1() {
+		return prodcut1;
+	}
+
+	public void setProdcut1(Product[] prodcut1) {
+		this.prodcut1 = prodcut1;
+	}
+	
+	
+	
+}
+
